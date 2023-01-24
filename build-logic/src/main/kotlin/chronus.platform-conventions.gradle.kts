e7c1c8bde4db5725ff116abe3e634cee66d5ec41
Siroshun09 <siro.shun8@gmail.com>
2023-01-24 13:15:15 +0900

    build(platform): relocate event4j

--- build-logic/src/main/kotlin/chronus.platform-conventions.gradle.kts
+++ build-logic/src/main/kotlin/chronus.platform-conventions.gradle.kts
@@ -39,5 +39,6 @@ tasks {
         val groupId = project.group
         relocate("org.jetbrains", "$groupId.libs.jetbrains")
         relocate("org.intellij", "$groupId.libs.intellij")
+        relocate("com.github.siroshun09.event4j", "$groupId.libs.event4j")
     }
 }
