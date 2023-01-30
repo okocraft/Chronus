e4c1433214ec3fb2500658285d19c64b218a333a
Siroshun09 <siro.shun8@gmail.com>
2023-01-30 10:26:44 +0900

    chore(paper): add more information about showing error at alias(libs.~~) [ci skip]

--- platform/paper/versions/paper-1_19_3/build.gradle.kts
+++ platform/paper/versions/paper-1_19_3/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("chronus.platform-conventions")
+    // This will show as an error on the IDE, but can be compiled successfully.
+    // See https://github.com/gradle/gradle/issues/22797
     alias(libs.plugins.paperweight.userdev)
 }
 
